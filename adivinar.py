c25aed99fd6a6801ccd51f8fb85d3bc7dc4e6f5b
Cauerio <cauefiorini123@gmail.com>
2023-11-30 18:16:13 +0100

    adivinar.py

--- adivinar.py
+++ adivinar.py
@@ -23,18 +23,11 @@ class Menu ():
 
         global opcion
         opcion = int(input("Elige una opción: "))
-        while True:
-            try:
-                opcion = int(input("Ingresa un número (1, 2 o 3): "))
-            except ValueError:
-                print("Por favor, ingresa un número válido.")
-            continue  # Regresa al inicio del bucle para repetir la pregunta
 
-            if opcion in (1, 2, 3):
-                break  # Sale del bucle si la opción es válida
-            else:
-                print("La opción no es válida. Debe ser 1, 2 o 3.")
-                os.system('cls' if os.name == 'nt' else 'clear')
+        while opcion != 1 and opcion != 2 and opcion != 3:
+            print ("Introduce un número de 1 al 3")
+            Menu.mostrar_menu()
+            os.system('cls' if os.name == 'nt' else 'clear')
         match (opcion):
             case 1:
                 print (Partido.iniciar_partida)
@@ -57,10 +50,9 @@ class Partido ():
         global num_random
         global num_intr
         num_random = random.randint (1, 10)
-
-        try:    
-            num_intr = int(input("Inserte un num de 1 al 10: "))
-        except ValueError:
+    
+        num_intr = int(input("Inserte un num de 1 al 10: "))
+        if opcion != 1 and opcion != 2 and opcion != 3 and opcion != 4 and opcion != 5 and opcion != 6 and opcion != 7 and opcion != 8 and opcion != 9 and opcion != 10:
             print ("Por favor, ingresa un número válido.")
         else:
             puntos = Partido.ranking(num_intr, num_random)
@@ -72,24 +64,34 @@ class Partido ():
 
         if diferencia == 0:
             puntos = 10  # Máxima puntuación si el número es exacto
+            print ("Has sumado 10 puntos")
         elif diferencia <= 1:
             puntos = 9   # Puntuación intermedia si está cerca (por ejemplo, a 2 números de diferencia)
+            print ("Has sumado 9 puntos")
         elif diferencia <= 2:
             puntos = 8
+            print ("Has sumado 8 puntos")
         elif diferencia <= 3:
             puntos = 7
+            print ("Has sumado 7 puntos")
         elif diferencia <= 4:
             puntos = 6
+            print ("Has sumado 6 puntos")
         elif diferencia <= 5:
             puntos = 5
+            print ("Has sumado 5 puntos")
         elif diferencia <= 6:
             puntos = 4
+            print ("Has sumado 4 puntos")
         elif diferencia <= 7:
             puntos = 3
+            print ("Has sumado 3 puntos")
         elif diferencia <= 8:
             puntos = 2
+            print ("Has sumado 2 puntos")
         elif diferencia <= 9:
             puntos = 1
+            print ("Has sumado 1 punto")
         else:
             puntos = 0   # Sin puntos si la diferencia es mayor
 
@@ -97,10 +99,9 @@ class Partido ():
     
 
 
-
 if __name__=='__main__':
     
     Menu.append()
     Menu.mostrar_menu()
     Partido.iniciar_partida()
-    Partido.calc_puntos
+    Partido.ranking()
