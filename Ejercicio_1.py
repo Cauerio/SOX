26ce1292bf0bcfb141c5da48f031b602e7b44c93
Cauerio <145791894+Cauerio@users.noreply.github.com>
2024-01-08 12:00:06 +0100

    Update Ejercicio_1.py

--- Ejercicio_1.py
+++ Ejercicio_1.py
@@ -1,42 +1,60 @@
 import os
+import time
+from datetime import date 
 
 class Cuenta():
     def __init__(self):
         self.dni = int(input ("Digite su DNI: "))
-        self.interes_diario = 0,25 
-        self.saldo = int(input ("Ingrese una cantidad inicial de saldo: "))
-        self.saldo_total = self.saldo
+        self.interes_diario = 0.025
+        self.interes_anual = self.interes_diario * 365
+        self.saldo_total = int(input ("Ingrese una cantidad inicial de saldo: "))
+        
         
     def menu(self):
-        print ("1) Visualizar saldo")
+        print ("1) Visualizar cuenta")
         print ("2) Ingresar ingresos")
         print ("3) Sacar ingresos")
-        seleccion = input ("Que operación deseas hacer?(Seleccione un numero):")
-        os.system('cls' if os.name == 'nt' else 'clear')
-        while seleccion != 1 or seleccion != 2 or seleccion != 3:
-            if seleccion == 1: 
-                print (self.saldo_total)
-            elif seleccion == 2: 
-                self.ingreso = int(input ("Cuanto quieres ingresar?: "))
-            else:
-                self.saque = int(input ("Cuanto quieres sacar?: "))
+        seleccion = int(input ("Que operación deseas hacer?(Seleccione un numero): "))
+        match(seleccion):
+            case 1:
+                print ("DNI: ",self.dni)
+                print ("Tu saldo total es: ",self.saldo_total, "€")
+                print ("Interés anual ",self.interes_anual) 
+            case 2: 
+                Cuenta.ingresar(self)
+            case 3:
+                Cuenta.sacar(self)
                 
     
-    def actualizar_saldo(self):
+    def ingresar(self):
+        self.ingreso = int(input ("Cuanto quieres ingresar?: "))
         print ("Actualizando...")
-        if c.menu.seleccion == 2:
-            self.saldo_total = self.saldo + self.ingreso
-            print ("Cantidad total de saldo: ",self.saldo_total)
-        elif c.menu.seleccion == 3:
-            self.saldo_total = self.saldo + self.saque
-            print ("Cantidad total de saldo: ", self.saldo_total)
-
-
+        time.sleep(3)
+        self.saldo_total = self.saldo_total + self.ingreso
+        print ("Cantidad total de saldo: ",self.saldo_total)
         
-            
+        
+    def sacar(self):
+        self.saque = int(input ("Cuanto quieres sacar?: "))
+        print ("Actualizando...")
+        time.sleep(3)
+        self.saldo_total = self.saldo_total - self.saque
+        print ("Cantidad total de saldo: ", self.saldo_total)
+
+    def actualizar_saldo(self):
+        today = date.today()
+        self.saldo_total_dia = (self.saldo_total*self.interes_diario)
+        self.saldo_total = (self.saldo_total - self.saldo_total_dia)    
+        
+        
+        
         
         
 if __name__=='__main__':
     c= Cuenta()
     c.menu()
+    c.actualizar_saldo()
+    
+    
+    
     
